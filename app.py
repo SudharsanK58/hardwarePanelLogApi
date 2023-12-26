53ef83d8f58d1875444446a3f4778c9f1fdca987
SudharsanK58 <sudharsan58@gmail.com>
2023-12-26 13:28:00 +0000

    Add smaple hosiptal APIs

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request
 from pymongo import MongoClient
 from fastapi.middleware.cors import CORSMiddleware
 from datetime import datetime, timedelta
@@ -19,6 +19,8 @@ client = MongoClient("mongodb+srv://ZusanMongo:Sadhana5823@zusandb.17e2qjw.mongo
 db = client.O_BeaconData
 collection = db.TicketLog
 device_collection = db.DeviceLog  # New collection
+attendants_collection = db.Attendants
+patient_details_collection = db.patientDetails
 
 
 @app.get("/device_log_data")
@@ -159,5 +161,153 @@ async def get_today_data_ticket_id(ticket_id: int):
             today_data.append(formatted_data)
 
         return today_data
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@app.post("/register_attendant")
+async def register_attendant(request: Request):
+    try:
+        # Access the request body directly
+        data = await request.json()
+
+        # Extract name and password from the request body
+        name = data.get("name")
+        password = data.get("password")
+
+        # Check if the attendant with the same name already exists
+        existing_attendant = attendants_collection.find_one({"name": name})
+        if existing_attendant:
+            raise HTTPException(status_code=400, detail="Attendant with this name already exists")
+
+        # Insert the new attendant into the collection
+        result = attendants_collection.insert_one({"name": name, "password": password})
+
+        # Check if the insertion was successful
+        if result.inserted_id:
+            return {"message": "Attendant registered successfully"}
+
+        raise HTTPException(status_code=500, detail="Failed to register attendant")
+
+    except HTTPException as http_exc:
+        # Forward HTTPException with custom detail message
+        raise http_exc
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@app.post("/login")
+async def login(request: Request):
+    try:
+        # Access the request body directly
+        data = await request.json()
+
+        # Extract name and password from the request body
+        name = data.get("name")
+        password = data.get("password")
+
+        # Check if the attendant with the provided name exists
+        attendant = attendants_collection.find_one({"name": name})
+
+        if attendant and attendant["password"] == password:
+            return {"message": "Login successful"}
+        else:
+            raise HTTPException(status_code=401, detail="Invalid credentials")
+
+    except HTTPException as http_exc:
+        # Forward HTTPException with custom detail message
+        raise http_exc
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@app.post("/save_patient_details")
+async def save_patient_details(request: Request):
+    try:
+        # Access the request body directly
+        data = await request.json()
+
+        # Extract patient details from the request body
+        patient_details = {
+            "Name": data.get("name"),
+            "Age": data.get("age"),
+            "Gender": data.get("gender"),
+            "City": data.get("city"),
+            "Height": data.get("height"),
+            "Weight": data.get("weight"),
+            "PhoneNumber": data.get("phone_number"),
+        }
+
+        # Get the latest patient ID
+        latest_patient = patient_details_collection.find_one(sort=[("PatientId", -1)])
+        latest_patient_id = latest_patient["PatientId"] if latest_patient else 1000
+
+        # Increment patient ID for the new patient
+        new_patient_id = latest_patient_id + 1
+
+        # Save patient details in the collection
+        result = patient_details_collection.insert_one({"PatientId": new_patient_id, **patient_details})
+
+        # Check if the insertion was successful
+        if result.inserted_id:
+            return {"PatientId": new_patient_id}
+
+        raise HTTPException(status_code=500, detail="Failed to save patient details")
+
+    except HTTPException as http_exc:
+        # Forward HTTPException with custom detail message
+        raise http_exc
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@app.post("/save_disease_history")
+async def save_disease_history(request: Request):
+    try:
+        # Access the request body directly
+        data = await request.json()
+
+        # Extract patient ID and disease history from the request body
+        patient_id = data.get("patient_id")
+        disease_history = {
+            "Diabetes": data.get("diabetes"),
+            "HeartAttack": data.get("heart_attack"),
+            "Paralysis": data.get("paralysis"),
+            "Gangrene": data.get("gangrene"),
+            "BloodPressure": data.get("blood_pressure"),
+            "BreathingDifficulty": data.get("breathing_difficulty"),
+        }
+
+        # Find the patient by ID
+        patient = patient_details_collection.find_one({"PatientId": patient_id})
+
+        if not patient:
+            raise HTTPException(status_code=404, detail="Patient not found")
+
+        # Update the patient's disease history with the new data and Heart Health
+        heart_health = (
+            disease_history.get("Diabetes", 0) * 1 +
+            disease_history.get("HeartAttack", 0) * 2 +
+            disease_history.get("Paralysis", 0) * 3 +
+            disease_history.get("Gangrene", 0) * 4 +
+            disease_history.get("BloodPressure", 0) * 5 +
+            disease_history.get("BreathingDifficulty", 0) * 6
+        )
+        
+        disease_history["HeartHealth"] = heart_health
+
+        patient_details_collection.update_one(
+            {"PatientId": patient_id},
+            {"$set": {"disease": disease_history}},
+        )
+
+        # Return the Heart Health value in the response
+        return {"HeartHealth": heart_health}
+
+    except HTTPException as http_exc:
+        # Forward HTTPException with custom detail message
+        raise http_exc
+
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
